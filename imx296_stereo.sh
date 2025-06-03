ebb29422961d7291eb6aac95b660b51b1c705fe8
admin <engine25vo@gmail.com>
2025-06-03 15:57:55 -0700

    final archive

--- imx296_stereo.sh
+++ imx296_stereo.sh
@@ -15,7 +15,7 @@
 . imx296_constants.sh
 
 # Valid values: file, rtsp or screen
-OUTPUT=file
+OUTPUT=rtsp
 
 if [ $OUTPUT == "rtsp" ]; then
         GST_OUTPUT="x264enc speed-preset=veryfast tune=zerolatency ! h264parse ! rtspclientsink location=rtsp://localhost:8554/stereo"
